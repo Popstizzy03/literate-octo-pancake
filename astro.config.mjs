f0ba60663f786650c8195426e529349378db5dbd
Rabboni Kabongo <kabongorabboni03@gmail.com>
2025-12-03 12:24:33 +0200

    fix: correction of astro config syntax errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import svelte from "@astrojs/svelte";
+
+import mdx from "@astrojs/mdx";
+
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
   integrations: [svelte(), mdx()],
   adapter: vercel()
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
